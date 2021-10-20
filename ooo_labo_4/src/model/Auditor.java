0b2b731504c0ed831ee2b8c96fc7745742e93534
Dieter Van der Zwalmen <dieter.vanderzwalmen@student.ucll.be>
2021-10-20 14:53:10 +0200

    14:53

--- ooo_labo_4/src/model/Auditor.java
+++ ooo_labo_4/src/model/Auditor.java
@@ -1,6 +1,18 @@
 package model;
 
-public class Auditor implements Observer{
+public class Auditor implements Observer{//auditor is een observer ("implements observer")
+    private Subject bank;
+    private String naam;//naam van de auditor zelf?
+
+    public Auditor(String naam, Subject bank){
+        this.setNaam(naam);
+        this.bank = bank;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
     @Override
     public void update() {
 
